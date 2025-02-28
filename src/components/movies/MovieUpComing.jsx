8598cff0f17da5a11e73ca01189f536e94e9ceb9
rizalsuryana <rizal.suryn@gmail.com>
2025-03-01 04:46:22 +0700

    ...

--- src/components/movies/MovieUpComing.jsx
+++ src/components/movies/MovieUpComing.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../utils/api';
+import MovieCard from '../MovieCard';
 
 const MovieUpComing = () => {
+  const [upcomingMovie, setUpcomingMovie] = useState([]);
+
+  useEffect(()=> {
+    const fetchUpcomingMovie = async () => {
+      try {
+        const _upcomingMovies = await api.getUpComingMovies();
+        setUpcomingMovie(_upcomingMovies.results);
+      } catch (error) {
+        console.error('error fetch upcoming movie', error);
+      }
+    };
+    fetchUpcomingMovie();
+  }, []);
   return (
-    <div>MovieUpComing</div>
+    <div>
+      <h1>Upcoming Movies</h1>
+      <div>
+        {
+          upcomingMovie.map((movie)=> (
+            <MovieCard key={movie.id} movie={movie}/>
+          ))
+        }
+      </div>
+
+    </div>
   );
 };
 
