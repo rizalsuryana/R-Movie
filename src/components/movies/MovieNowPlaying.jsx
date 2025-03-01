b99d0339b4f160f01288d292ac062df1bf8f0326
rizalsuryana <rizal.suryn@gmail.com>
2025-03-02 02:24:03 +0700

    ..

--- src/components/movies/MovieNowPlaying.jsx
+++ src/components/movies/MovieNowPlaying.jsx
@@ -1,32 +1,54 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import api from '../../utils/api';
 import MovieCard from '../MovieCard';
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 80px auto 20px; /* Beri jarak dari navbar yang fixed */
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const MovieGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 20px;
+  justify-content: center;
+`;
 
 const MovieNowPlaying = () => {
   const [nowPlayingMovie, setNowPlayingMovie] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchNowPlayingMovie = async () => {
       try {
         const nowPlayingMovie = await api.getNowPlayingMovies();
-
         setNowPlayingMovie(nowPlayingMovie.results);
       } catch (error) {
-        console.error('error fetch now playing', error);
+        console.error('Error fetching now playing movies:', error);
       }
     };
+
     document.title = 'R-Movie | Now Playing Movie';
     fetchNowPlayingMovie();
   }, []);
+
   return (
-    <div>
-
-      <h1>Now Playing</h1>
-      {
-        nowPlayingMovie.map((movie)=> <MovieCard key={movie.id} movie={movie}/>)
-      }
-
-    </div>
+    <Container>
+      <Title>Now Playing</Title>
+      <MovieGrid>
+        {nowPlayingMovie.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </MovieGrid>
+    </Container>
   );
 };
 
